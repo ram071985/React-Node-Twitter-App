fc015086187652f782ff74b0c20fef43aff54c0b
ram071985 <reidmuchow@gmail.com>
2020-03-12 18:13:42 -0500

    added onclick to twitter seacrch api logo in nav

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css'
+import SplashPage from "./SplashPage.js";
 
 function NavBar() {
   return (
     <Container>
       <Navbar className="color-nav" fixed="top" expand="lg" variant="dark">
-        <Navbar.Brand href="#home">Twitter Search Api</Navbar.Brand>
+        <Navbar.Brand onClick={<SplashPage />} href="#home">Twitter Search Api</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
