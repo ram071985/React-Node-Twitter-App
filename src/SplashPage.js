d242c02d505c2a77150050264714ac9015028a11
ram071985 <reidmuchow@gmail.com>
2020-03-12 12:59:02 -0500

    adding style tage to react bootstrap Image.

--- src/SplashPage.js
+++ src/SplashPage.js
@@ -2,19 +2,18 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Jumbotron, Button } from "react-bootstrap";
 import './index.css'
+import colImage from './SUNRISE-BUILDING-PHOTOGRAPHY-809801.jpeg';
 
 
 
-let sectionStyle = {
-  backgroundImage: `url(${Background})`
-};
 
 function SplashPage() {
+  const imageUrl = window.innerWidth >= 650 ? colImage : colImage;
   return (
     <Container>
       <br />
       <br />
-      <Jumbotron className="mt-5 jumbotron">
+      <Jumbotron className="mt-5 jumbotron" style={{backgroundImage: `url(${imageUrl})`}}>
         <h1 className="headline">Welcome To My Twitter Search Api</h1>
         <p>
           This is a simple hero unit, a simple jumbotron-style component for
