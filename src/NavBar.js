55abe91834176a097ed5d55f5adafab8d0234cd2
ram071985 <reidmuchow@gmail.com>
2020-03-13 14:10:16 -0500

    changed splash page layout by removing background image and added tagline.

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css'
-import SplashPage from "./SplashPage.js";
 
 function NavBar() {
   return (
     <Container>
       <Navbar className="color-nav" fixed="top" expand="lg" variant="dark">
-        <Navbar.Brand onClick={<SplashPage />} href="#home">Twitter Search Api</Navbar.Brand>
+        <Navbar.Brand  href="#home">Twitter Search Api</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
--- src/SplashPage.js
+++ src/SplashPage.js
@@ -2,22 +2,19 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import "./index.css";
-import colImage from "./SILHOUETTE-PHOTOGRAPHY-OF-CLOUDS-692116.jpeg";
-import twitterContainer from "./Twitter_Logo_WhiteOnImage.png";
+import twitterContainer from "./clipart-2-13-12-55-41.png";
 
 function SplashPage() {
   return (
-    <Container fluid>
-      <br />
-      <br />
-      <br />
+    <Container className="container" fluid>
       <Row>
-        <Col >
-        <h1 className="top-left" id="header-text">Welcome To Twitter Search Api</h1>
-        <Image className="mt-5 jumbotron" src={colImage} fluid />
-        <Image className="twitter-logo" src={twitterContainer} />
+        <Col className="d-inline mt-5 mb-5">
+        <h1>Welcome To Twitter Search Api</h1>
+        <h7 className="d-block describe"><strong>Search</strong> for user and topic tweets user or view a <strong>random</strong> tweet from one of my five favorite twitter users.</h7>
+        <Image className="d-inline mt-3 mb-4 ml-5 twitter-logo" src={twitterContainer} />
         </Col>
       </Row>
+    
     </Container>
   );
 }
