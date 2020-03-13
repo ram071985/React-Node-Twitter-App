c7783fe12a850af988e36f51bc3297da77d74ba7
ram071985 <reidmuchow@gmail.com>
2020-03-13 15:33:27 -0500

    added navbar to search page

--- src/Search.js
+++ src/Search.js
@@ -2,15 +2,17 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import './index.css';
+import NavBar from './NavBar.js'
 
 class Search extends Component {
   render() {
     return (
+    <NavBar />
         <Container>
             <Row>
                 <Col>
                 <Form.Group>
-                <Form.Control type="text" placeholder="Normal text" />
+                <Form.Control type="text" placeholder="Search for @username or topic..." />
                 </Form.Group>
                 </Col>
             </Row>
