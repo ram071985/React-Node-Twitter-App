3d4d2956da3a364c704eb26d21411290a4bd8596
ram071985 <reidmuchow@gmail.com>
2020-03-15 11:31:06 -0500

    search page is not loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import NavBar from './NavBar.js';
-import SplashPage from './SplashPage.js';
 import {Container} from "react-bootstrap"
 
 function App() {
   return (
 <Container >
    <NavBar />
-   <SplashPage />
 </Container>
   );
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -11,19 +11,19 @@ export default function NavBar() {
     <Router>
       <Container>
         <Navbar className="color-nav" fixed="top" expand="lg" variant="dark">
-          <Navbar.Brand as={link} to="/">
+          <Navbar.Brand as={Link} to="/">
             Twitter Search Api
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link as={link} to="/">
+              <Nav.Link as={Link} to="/">
                 Home
               </Nav.Link>
-              <Nav.Link as={link} to="/Search">
+              <Nav.Link as={Link} to="/Search">
                 Search
               </Nav.Link>
-              <Nav.Link as={link} to="/Random">
+              <Nav.Link as={Link} to="/Random">
                 Random
               </Nav.Link>
             </Nav>
@@ -37,7 +37,7 @@ export default function NavBar() {
              <Search />
           </Route>
           <Route path="/Random">
-
+             
           </Route>
         </Switch>
       </Container>
