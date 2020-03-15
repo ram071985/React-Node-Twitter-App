53d0e75561badecd23dbc19d693516c8bb501bcc
ram071985 <reidmuchow@gmail.com>
2020-03-15 11:17:31 -0500

    added splash page and search to route paths and imported splah page to NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,26 +1,46 @@
 import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './index.css';
-import Search from './Search.js';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import "./index.css";
+import Search from "./Search.js";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import SplashPage from "./SplashPage.js"
 
 export default function NavBar() {
   return (
-   <Router>
-   <Container>
-      <Navbar className="color-nav" fixed="top" expand="lg" variant="dark">
-        <Navbar.Brand as={link} to='/'>Twitter Search Api</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link as={link} to="/">Home</Nav.Link>
-  <Nav.Link as={link} to='/Search'>Search</Nav.Link>
-            <Nav.Link as={link} to="/Random">Random</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-</Container>
-  </Router>
-  )
+    <Router>
+      <Container>
+        <Navbar className="color-nav" fixed="top" expand="lg" variant="dark">
+          <Navbar.Brand as={link} to="/">
+            Twitter Search Api
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link as={link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={link} to="/Search">
+                Search
+              </Nav.Link>
+              <Nav.Link as={link} to="/Random">
+                Random
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Switch>
+          <Route exact path="/">
+             <SplashPage />
+          </Route>
+          <Route path="/Search">
+             <Search />
+          </Route>
+          <Route path="/Random">
+
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
+  );
 }
