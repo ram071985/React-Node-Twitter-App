8d383305910537d7d40461a578c4301b6c16d44b
ram071985 <reidmuchow@gmail.com>
2020-03-14 19:10:00 -0500

    added router code.

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Form } from "react-bootstrap";
 import './index.css';
 import NavBar from './NavBar.js'
 
