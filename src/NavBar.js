263996edf75638025cfdf6de94e9c20ff4795fe8
ram071985 <reidmuchow@gmail.com>
2020-03-15 10:52:01 -0500

    added router to index.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,12 +3,15 @@ import { Container, Navbar, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
 import Search from './Search.js';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 function NavBar() {
   return (
+    <Router>
     <Container>
+    
       <Navbar className="color-nav" fixed="top" expand="lg" variant="dark">
-        <Navbar.Brand  href="">Twitter Search Api</Navbar.Brand>
+        <Navbar.Brand  <Link to="/SplashPage.js">Twitter Search Api</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
@@ -16,9 +19,11 @@ function NavBar() {
   <Nav.Link onClick={<Search />}>Search</Nav.Link>
             <Nav.Link href="#link">Random</Nav.Link>
           </Nav>
+          <Switch>
         </Navbar.Collapse>
       </Navbar>
     </Container>
+    </Router>
   );
 }
 
