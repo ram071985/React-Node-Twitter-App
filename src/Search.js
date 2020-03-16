bbe316d5922374aff64f958053a4770d776018c0
ram071985 <reidmuchow@gmail.com>
2020-03-16 15:35:21 -0500

    added twiiter row, name header and circle image to twitter col element

--- src/Search.js
+++ src/Search.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import './index.css';
+import Reid from "./images/reid.png";
 
 class Search extends Component {
   render() {
@@ -14,11 +15,17 @@ class Search extends Component {
                 <Form.Group>
                 <Form.Control className="control-form" type="input" placeholder="Search Twitter..." />
                 </Form.Group>
-                <Button as="input" type="button" value="Seach By User Handle" />
+                <Button as="input" type="button" value="Search By User Handle" />
                 <Button className="d-inline ml-5 topic-button" as="input" type="button" value="Search By Topic" />
                 </Col>
             </Row>
             <br />
+            <Row className="tweet-row">
+              <Col>
+              <Image src={Reid} />
+              <h6 className="real-name ml-5 mt-3">Reid Muchow</h6>
+              </Col>
+            </Row>
             <br />
         </Container>
     )
