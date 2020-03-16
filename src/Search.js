5a226c898ecffcb90690053730d128672a7a29fb
ram071985 <reidmuchow@gmail.com>
2020-03-16 13:43:34 -0500

    fixed margins and changed display from inline to block on the topic button of the search page.

--- src/Search.js
+++ src/Search.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col, Form } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import './index.css';
 
 class Search extends Component {
   render() {
     return (
         <Container>
+          <br />
+          <br />
             <Row>
                 <Col>
                 <Form.Group>
-                <Form.Control type="text" placeholder="Search for @username or topic..." />
+                <Form.Control className="control-form" type="input" placeholder="Search Twitter..." />
                 </Form.Group>
+                <Button as="input" type="button" value="Seach By User Handle" />
+                <Button className="d-inline ml-5 topic-button" as="input" type="button" value="Search By Topic" />
                 </Col>
             </Row>
+            <br />
+            <br />
         </Container>
     )
   }
