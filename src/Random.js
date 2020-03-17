73e004bd9191c92308f44dbf2792576ebae354bb
ram071985 <reidmuchow@gmail.com>
2020-03-17 17:12:58 -0500

    finished random page.

--- src/Random.js
+++ src/Random.js
@@ -17,41 +17,59 @@ class Random extends Component {
         </Row>
         <Row>
           <Col className=" column-one">
-            <button className="btn btn-default" size="lg">
+            <button className="btn btn-warning" size="lg">
             <Image className="d-inline mr-2 reid-random" src={Reid} />
             <h6 className="d-inline real-name-random-one text-left"><strong>Reid Muchow</strong></h6>
             <p className="d-block user-name-random ml-5">@ReidMuchow</p>
             </button>
           </Col>
           <Col className="column-two">
-          <button className="btn btn-default" size="lg">
+          <button className="btn btn-warning" size="lg">
             <Image className="d-inline mr-2 reid-random" src={Reid} />
             <h6 className="d-inline real-name-random-one text-left"><strong>Reid Muchow</strong></h6>
             <p className="d-block user-name-random ml-5">@ReidMuchow</p>
             </button>
           </Col>
           <Col className="col-4-3">
-          <button className="btn btn-default" size="lg">
+          <button className="btn btn-warning" size="lg">
             <Image className="d-inline mr-2 reid-random" src={Reid} />
             <h6 className="d-inline real-name-random-one text-left"><strong>Reid Muchow</strong></h6>
             <p className="d-block user-name-random ml-5">@ReidMuchow</p>
             </button>
           </Col>
           <Col className="col-4-4">
-          <button className="btn btn-default" size="lg">
+          <button className="btn btn-warning" size="lg">
             <Image className="d-inline mr-2 reid-random" src={Reid} />
             <h6 className="d-inline real-name-random-one text-left"><strong>Reid Muchow</strong></h6>
             <p className="d-block user-name-random ml-5">@ReidMuchow</p>
             </button>
           </Col>
           <Col className="col-4-5">
-          <button className="btn btn-default" size="lg">
+          <button className="btn btn-warning" size="lg">
             <Image className="d-inline mr-2 reid-random" src={Reid} />
             <h6 className="d-inline real-name-random-one text-left"><strong>Reid Muchow</strong></h6>
             <p className="d-block user-name-random ml-5">@ReidMuchow</p>
             </button>
           </Col>
         </Row>
+        <br />
+        <br />
+        <br />
+      
+
+        <Row className="tweet-row">
+              <Col>
+              <Image className="d-inline mt-1 mr-1 ml-1" src={Reid} />
+              <h6 className="d-inline real-name ml-1 mt-1"><strong>Reid Muchow</strong></h6>
+              <p className="d-inline ml-2 user-handle">@ReidMuchow</p>
+              <p className="d-block tweet-text">Hey all, I feel like I've been living under a rock.  It's been ages since I've been on twitter.</p>
+              <Image className="d-block retweet-image" src={ReTweet}/>
+              <p className="retweet-num">20</p>
+              <Image className="like-image" src={Like} />
+              <p className="like-num">15</p>
+              </Col>
+            </Row>
+            <br />
       </Container>
     );
   }
