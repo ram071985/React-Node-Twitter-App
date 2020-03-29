c121098b3b3de67c1544bedeab69936e6cb1cdd1
ram071985 <reidmuchow@gmail.com>
2020-03-29 14:05:31 -0500

    added path variable for path.resolve server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
 
 
 
