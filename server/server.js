2d94e823884e86aa5733b61fb4b645be2be0d1fc
ram071985 <reidmuchow@gmail.com>
2020-03-29 11:04:42 -0500

    tried to change server.js app get method to route to build but it's not working.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ const port = 3000
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('root'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
