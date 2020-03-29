fc65c899f4d9ee7d0bc1249f44e7874997f57abb
ram071985 <reidmuchow@gmail.com>
2020-03-29 10:41:58 -0500

    changed route of app.get method in server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ const port = 3000
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/client/build', (req, res) => res.send('index.html'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
