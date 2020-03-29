1960c02374323556b2f59a69900c769e30bd3d03
ram071985 <reidmuchow@gmail.com>
2020-03-29 11:25:36 -0500

    stuck

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ const port = 3000
 
 
 
-app.get('/', (req, res) => res.send('root'));
+app.get('/', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
