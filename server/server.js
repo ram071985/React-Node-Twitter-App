3a50f3a303b88b1f4e0c5fc73df2316ade479bf2
ram071985 <reidmuchow@gmail.com>
2020-03-30 16:38:47 -0500

    added class component to server that sends get request to twitter api.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const path = require('path');
+const path = require("path");
+import axios from "axios";
 
 
-
-app.use('/', express.static(path.join(__dirname, '../client/build')));
+app.use("/", express.static(path.join(__dirname, "../client/build")));
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
-})
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+class TwitterSearch extends Component {
+    async componentDidMount() {
+      const twitterResponse = await axios.get(
+        "https://api.twitter.com/1.1/search/tweets.json"
+      );
+      console.log(twitterResponse);
+    }
+  }
+  
+  export default TwitterSearch;
