2d7734d6bbe70c63e1cb31069a131485daaf100c
ram071985 <reidmuchow@gmail.com>
2020-03-31 14:19:24 -0500

    added console.log to script.js and added get request to server.js

--- server/server.js
+++ server/server.js
@@ -2,19 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
-const axios = require("axios");
+//const axios = require("axios");
 
 app.use("/", express.static(path.join(__dirname, "../client/build")));
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
+app.get('/api/twitter', (req, res) => {
+    res.send(twitter);
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-axios
-  .get("https://api.twitter.com/1.1/search/tweets.json")
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+const twitter = {
+ "name" : "Eric Andre"
+}
