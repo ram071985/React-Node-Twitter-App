72e532bec0c815414ba90c5ef7d3de7fd5698bc2
ram071985 <reidmuchow@gmail.com>
2020-04-03 10:47:18 -0500

    created config function for authorization header and bearer token.

--- server/server.js
+++ server/server.js
@@ -10,12 +10,16 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
+config = () => {
+  headers: {
+    Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAO23DAEAAAAA0UvE62fUsmqWWFM3F3xuCh5QFAY%3D9CVBej4ed4zQYI0sxVxHSa2m8ILnOQ5W2AegDWjJo0yBvgg1VV'
+  }
+}
 app.get("/api/tweet/random", (req, res) => {
-  axios.get('https://api.twitter.com/1.1/search/tweets.json?screen-name=ericandre').then((twitterRes, error) => {
-     headers: {
-       'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAO23DAEAAAAA0UvE62fUsmqWWFM3F3xuCh5QFAY%3D9CVBej4ed4zQYI0sxVxHSa2m8ILnOQ5W2AegDWjJo0yBvgg1VV' }
-     }
-  });
+  axios.get('https://api.twitter.com/1.1/search/tweets.json?screen-name=ericandre', config
+  )
+  .then(res => console.log(res)) 
+  .catch(err => console.error(err));
 
   
 });
