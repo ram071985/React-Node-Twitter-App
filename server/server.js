bf446c9c42c099c59c2b96151fa76bb73942042c
ram071985 <reidmuchow@gmail.com>
2020-04-03 18:11:02 -0500

    changed location og const config.

--- server/server.js
+++ server/server.js
@@ -6,21 +6,24 @@ const axios = require("axios");
 
 app.use("/", express.static(path.join(__dirname, "../client/build")));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
-config = () => {
-  headers: {
-    Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAO23DAEAAAAA0UvE62fUsmqWWFM3F3xuCh5QFAY%3D9CVBej4ed4zQYI0sxVxHSa2m8ILnOQ5W2AegDWjJo0yBvgg1VV'
-  }
-}
 app.get("/api/tweet/random", (req, res) => {
-  //send back a random tweet
-  const result = axios.get('https://api.twitter.com/1.1/search/tweets.json?q=ericandre', config);
-  res.send(result);
+  axios
+    .get("https://api.twitter.com/1.1/search/tweets.json", config)
+    .then(twitterResponse => {
+      const config = {
+        headers: {
+          Authorization:
+            "Bearer AAAAAAAAAAAAAAAAAAAAAO23DAEAAAAA0UvE62fUsmqWWFM3F3xuCh5QFAY%3D9CVBej4ed4zQYI0sxVxHSa2m8ILnOQ5W2AegDWjJo0yBvgg1VV"
+        }
+      };
+  
+      res.send(result);
+    })
+    .catch(err => console.error(err));
 });
 
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
