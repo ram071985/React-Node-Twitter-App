5f1857f38d771a6655aab2eaa6175b88324f3dd0
ram071985 <reidmuchow@gmail.com>
2020-04-04 10:12:59 -0500

    added twitterResponse to res.send.

--- server/server.js
+++ server/server.js
@@ -11,19 +11,21 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/tweet/random", (req, res) => {
+  const config = {
+    headers: {
+      Authorization:
+        "Bearer AAAAAAAAAAAAAAAAAAAAAO23DAEAAAAA0UvE62fUsmqWWFM3F3xuCh5QFAY%3D9CVBej4ed4zQYI0sxVxHSa2m8ILnOQ5W2AegDWjJo0yBvgg1VV"
+    }
+  };
   axios
-    .get("https://api.twitter.com/1.1/search/tweets.json", config)
+    .get("https://api.twitter.com/1.1/lists/memberships.json?screen_name=ericandre", config)
     .then(twitterResponse => {
-      const config = {
-        headers: {
-          Authorization:
-            "Bearer AAAAAAAAAAAAAAAAAAAAAO23DAEAAAAA0UvE62fUsmqWWFM3F3xuCh5QFAY%3D9CVBej4ed4zQYI0sxVxHSa2m8ILnOQ5W2AegDWjJo0yBvgg1VV"
-        }
-      };
   
-      res.send(result);
+      res.send(twitterResponse);
     })
-    .catch(err => console.error(err));
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
