dc3a5432afe43419fbff2928e4bc9ed23e9c107d
ram071985 <reidmuchow@gmail.com>
2020-04-05 16:43:30 -0500

    added dynamic tweet row generation with const tweetRows in render() and added in map for each part of the tweet card

--- client/src/Random.js
+++ client/src/Random.js
@@ -19,7 +19,7 @@ class Random extends Component {
     axios.get("/api/tweet/random/eckhart")
     .then(res => {
       this.setState({
-        user: res.data.statuses.user
+        user: res.data.statuses
       });
       console.log(this.state.user);
     });
@@ -28,24 +28,24 @@ class Random extends Component {
   render() {
 
     const tweetRows = this.state.user.map(users => (
-      <Row className="tweet-row">
-          <Col className="tweet-col">
-            <Image className="d-inline mt-1 mr-1 ml-1" src={Reid} />
-            <h6 className="d-inline real-name ml-1 mt-1">
-              <strong>{users.screen_name}</strong>
-            </h6>
-            <p className="d-inline ml-2 user-handle">@ReidMuchow</p>
-            <p className="d-block tweet-text">
-              Hey all, I feel like I've been living under a rock. It's been ages
-              since I've been on twitter.
-            </p>
-            <Image className="d-block retweet-image" src={ReTweet} />
-            <p className="retweet-num">20</p>
-            <Image className="like-image" src={Like} />
-            <p className="like-num">15</p>
-          </Col>
-        </Row>
+      <Container>
+      <Row className="d-block tweet-row">
+      <Col className="tweet-col">
+        <Image className="d-inline mt-1 mr-1 ml-1" src={Reid} />
+        <h6 className="d-inline real-name ml-1 mt-1">
+          <strong>{users.user.name}</strong>
+        </h6>
+        <p className="d-inline ml-2 user-handle">@{users.user.screen_name}</p>
+        <p className="d-block tweet-text">{users.text}</p>
+        <Image className="d-block retweet-image" src={ReTweet} />
+        <p className="retweet-num">{users.retweet_count}</p>
+        <Image className="like-image" src={Like} />
+        <p className="like-num">{users.favorite_count}</p>
+      </Col>
+      </Row>
+      </Container>
     ));
+    
     return (
       <Container className="container-random">
         <Row>
@@ -107,8 +107,8 @@ class Random extends Component {
         <br />
         <br />
         <br />
-
         
+        {tweetRows}
         <br />
       </Container>
     );
