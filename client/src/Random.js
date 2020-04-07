0e149204ba15233c6a326461c22286666d2f7a5e
ram071985 <reidmuchow@gmail.com>
2020-04-06 19:03:36 -0500

    added col to 100 percent height in columns on random.js and added questlove plus yuval harari to my list along with endpoints to server and axios get requests.

--- client/src/Random.js
+++ client/src/Random.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import "./index.css";
-import Reid from "./images/reid.png";
+import Eckhart from "./images/AUcTAQC7_normal.jpg";
+import Questlove from "./images/QIV6c_-D_normal.jpg";
 import ReTweet from "./images/retweet-action.png";
 import Like from "./images/like-action.png";
 import axios from "axios";
@@ -27,21 +28,45 @@ class Random extends Component {
     });
   };
 
-  render() {
+  getQuestlove = () => {
+    axios.get("/api/tweet/random/questlove")
+    .then(res => {
+      this.setState({
+        user: res.data.statuses
+      },
+      function() {
+      console.log(this.state.user);
+      });
+    });
+  };
 
+  getYuval = () => {
+    axios.get("/api/tweet/random/yuval")
+    .then(res => {
+      this.setState({
+        user: res.data.statuses
+      },
+      function() {
+      console.log(this.state.user);
+      });
+    });
+  };
+
+  
+  render() {
     const tweetRows = this.state.user.map(users => (
-      <Container>
+      <Container className="tweet-card">
       <Row className="d-block tweet-row">
       <Col className="tweet-col">
-        <Image className="d-inline mt-1 mr-1 ml-1" src={Reid} />
+        <Image className="d-inline mt-2 mr-1 ml-1" src={users.user.profile_image_url} />
         <h6 className="d-inline real-name ml-1 mt-1">
           <strong>{users.user.name}</strong>
         </h6>
         <p className="d-inline ml-2 user-handle">@{users.user.screen_name}</p>
-        <p className="d-block tweet-text">{users.text}</p>
+        <p className="d-block tweet-text">{users.full_text}</p>
         <Image className="d-block retweet-image" src={ReTweet} />
         <p className="retweet-num">{users.retweet_count}</p>
-        <Image className="like-image" src={Like} />
+        <Image className="d-block like-image" src={Like} />
         <p className="like-num">{users.favorite_count}</p>
       </Col>
       </Row>
@@ -50,62 +75,62 @@ class Random extends Component {
     
     return (
       <Container className="container-random">
-        <Row>
-          <Col>
-            <h4 className="text-center mt-3 choose-text">
-              Choose from one of my <strong>5</strong> favorite twitter users to
-              see <strong>10</strong> of their random tweets.
-            </h4>
-          </Col>
-        </Row>
-        <br />
-        <Row className="card-row">
-          <Col className="col-4">
-            <button className="d-block btn btn-warning" onClick={this.getEckhartTolle} size="lg">
-              <Image className="d-inline mr-2 reid-random" src={Reid} />
-              <h6 className="d-inline real-name-random-one text-left">
-                <strong>Reid Muchow</strong>
-              </h6>
-              <p className="d-block user-name-random ml-5">@ReidMuchow</p>
-            </button>
-          </Col>
-          <Col className="col-4">
-            <button className="d-block btn btn-warning" size="lg">
-              <Image className="d-inline mr-2 reid-random" src={Reid} />
-              <h6 className="d-inline real-name-random-one text-left">
-                <strong>Reid Muchow</strong>
-              </h6>
-              <p className="d-block user-name-random ml-5">@ReidMuchow</p>
-            </button>
-          </Col>
-          <Col className="col-4">
-            <button className="d-block btn btn-warning" size="lg">
-              <Image className="d-inline mr-2 reid-random" src={Reid} />
-              <h6 className="d-inline real-name-random-one text-left">
-                <strong>Reid Muchow</strong>
-              </h6>
-              <p className="d-block user-name-random ml-5">@ReidMuchow</p>
-            </button>
-          </Col>
-          <Col className="col-4">
-            <button className="d-block btn btn-warning" size="lg">
-              <Image className="d-inline mr-2 reid-random" src={Reid} />
-              <h6 className="d-inline real-name-random-one text-left">
-                <strong>Reid Muchow</strong>
-              </h6>
-              <p className="d-block user-name-random ml-5">@ReidMuchow</p>
-            </button>
-          </Col>
-          <Col className="col-4-5">
-            <button className="d-block btn btn-warning" size="lg">
-              <Image className="d-inline mr-2 reid-random" src={Reid} />
-              <h6 className="d-inline real-name-random-one text-left">
-                <strong>Reid Muchow</strong>
-              </h6>
-              <p className="d-block user-name-random ml-5">@ReidMuchow</p>
-            </button>
-          </Col>
-        </Row>
+      <Row>
+      <Col>
+        <h4 className="text-center mt-3 choose-text">
+          Choose from one of my <strong>5</strong> favorite twitter users to
+          see <strong>10</strong> of their random tweets.
+        </h4>
+      </Col>
+    </Row>
+    <br />
+    <Row className="card-row">
+      <Col className="col-4">
+        <button className="d-block btn btn-warning" onClick={this.getEckhartTolle} size="lg">
+          <Image className="d-inline mr-2 reid-random" src={Eckhart} />
+          <h6 className="d-inline real-name-random-one text-left">
+            <strong>Eckhart Tolle</strong>
+          </h6>
+          <p className="d-block user-name-random ml-5">@EckhartTolle</p>
+        </button>
+      </Col>
+      <Col className="col-4">
+        <button className="d-block btn btn-warning" onClick={this.getQuestlove} size="lg">
+          <Image className="d-inline mr-2 reid-random" src={Questlove} />
+          <h6 className="d-inline real-name-random-one text-left">
+            <strong>?NG Shamen</strong>
+          </h6>
+          <p className="d-block user-name-random ml-5">@questlove</p>
+        </button>
+      </Col>
+      <Col className="col-4">
+        <button className="d-block btn btn-warning" onClick={this.getYuval} size="lg">
+          <Image className="d-inline mr-2 reid-random" src={Eckhart} />
+          <h6 className="d-inline real-name-random-one text-left">
+            <strong>Yuval Noah Harari</strong>
+          </h6>
+          <p className="d-block user-name-random ml-5">@hurari_yuval</p>
+        </button>
+      </Col>
+      <Col className="col-4">
+        <button className="d-block btn btn-warning" size="lg">
+          <Image className="d-inline mr-2 reid-random" src={Eckhart} />
+          <h6 className="d-inline real-name-random-one text-left">
+            <strong>Reid Muchow</strong>
+          </h6>
+          <p className="d-block user-name-random ml-5">@ReidMuchow</p>
+        </button>
+      </Col>
+      <Col className="col-4-5">
+        <button className="d-block btn btn-warning" size="lg">
+          <Image className="d-inline mr-2 reid-random" src={Eckhart} />
+          <h6 className="d-inline real-name-random-one text-left">
+            <strong>Reid Muchow</strong>
+          </h6>
+          <p className="d-block user-name-random ml-5">@ReidMuchow</p>
+        </button>
+      </Col>
+    </Row>
         <br />
         <br />
         <br />
--- server/server.js
+++ server/server.js
@@ -18,7 +18,41 @@ app.get("/api/tweet/random/eckhart", (req, res) => {
     }
   };
   axios
-    .get("https://api.twitter.com/1.1/search/tweets.json?q=eckharttolle&result_type=popular&count=10", config)
+    .get("https://api.twitter.com/1.1/search/tweets.json?q=eckharttolle&screen_name=ekharttolle&result_type=popular&lang=en&tweet_mode=extended&count=10", config)
+    .then(twitterResponse => {
+  
+      res.send(twitterResponse.data);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+app.get("/api/tweet/random/questlove", (req, res) => {
+  const config = {
+    headers: {
+      Authorization:
+        "Bearer AAAAAAAAAAAAAAAAAAAAAO23DAEAAAAA0UvE62fUsmqWWFM3F3xuCh5QFAY%3D9CVBej4ed4zQYI0sxVxHSa2m8ILnOQ5W2AegDWjJo0yBvgg1VV"
+    }
+  };
+  axios
+    .get("https://api.twitter.com/1.1/search/tweets.json?q=questlove&result_type=popular&screen_name=questlove&lang=en&tweet_mode=extended&count=10", config)
+    .then(twitterResponse => {
+  
+      res.send(twitterResponse.data);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+app.get("/api/tweet/random/yuval", (req, res) => {
+  const config = {
+    headers: {
+      Authorization:
+        "Bearer AAAAAAAAAAAAAAAAAAAAAO23DAEAAAAA0UvE62fUsmqWWFM3F3xuCh5QFAY%3D9CVBej4ed4zQYI0sxVxHSa2m8ILnOQ5W2AegDWjJo0yBvgg1VV"
+    }
+  };
+  axios
+    .get("https://api.twitter.com/1.1/search/tweets.json?q=@harari_yuval&result_type=popular&lang=en&tweet_mode=extended&count=10", config)
     .then(twitterResponse => {
   
       res.send(twitterResponse.data);
