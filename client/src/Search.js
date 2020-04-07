ed859c92fc1e3e11bfd748887443cd9a731d2fe8
ram071985 <reidmuchow@gmail.com>
2020-04-07 15:11:37 -0500

    added constructor for state and items, functionality for input form onCHange event.

--- client/src/Search.js
+++ client/src/Search.js
@@ -2,13 +2,44 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col, Form, Button, Image } from "react-bootstrap";
 import './index.css';
-import Reid from "./images/reid.png";
 import ReTweet from "./images/retweet-action.png"
 import Like from "./images/like-action.png"
 
 class Search extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      query: [],
+      entry: ""
+    };
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+  this.setState({ [name]: value} );
+ console.log(this.state.entry);
+  };
+
   render() {
+    const tweetRows = this.state.query.map(users => (
+      <Container className="tweet-card">
+      <Row className="d-block tweet-row">
+      <Col className="tweet-col">
+        <Image className="d-inline mt-2 mr-1 ml-1" src={users.user.profile_image_url} />
+        <h6 className="d-inline real-name ml-1 mt-1">
+          <strong>{users.user.name}</strong>
+        </h6>
+        <p className="d-inline ml-2 user-handle">@{users.user.screen_name}</p>
+        <p className="d-block tweet-text">{users.full_text}</p>
+        <Image className="d-block retweet-image" src={ReTweet} />
+        <p className="retweet-num">{users.retweet_count}</p>
+        <Image className="d-block like-image" src={Like} />
+        <p className="like-num">{users.favorite_count}</p>
+      </Col>
+      </Row>
+      </Container>
+    ));
     return (
         <Container>
           <br />
@@ -16,25 +47,14 @@ class Search extends Component {
             <Row>
                 <Col>
                 <Form.Group>
-                <Form.Control className="control-form" type="input" placeholder="Search Twitter..." />
+                <Form.Control className="control-form" type="input" value={this.state.entry} name="entry" onChange={this.handleChange} placeholder="Search Twitter..." />
                 </Form.Group>
                 <Button as="input" type="button" value="Search By User Handle" />
                 <Button className="d-inline ml-5 topic-button" as="input" type="button" value="Search By Topic" />
                 </Col>
             </Row>
             <br />
-            <Row className="tweet-row">
-              <Col>
-              <Image className="d-inline mt-1 mr-1 ml-1" src={Reid} />
-              <h6 className="d-inline real-name ml-1 mt-1"><strong>Reid Muchow</strong></h6>
-              <p className="d-inline ml-2 user-handle">@ReidMuchow</p>
-              <p className="d-block tweet-text">Hey all, I feel like I've been living under a rock.  It's been ages since I've been on twitter.</p>
-              <Image className="d-block retweet-image" src={ReTweet}/>
-              <p className="retweet-num">20</p>
-              <Image className="like-image" src={Like} />
-              <p className="like-num">15</p>
-              </Col>
-            </Row>
+             {tweetRows}
             <br />
         </Container>
     )
