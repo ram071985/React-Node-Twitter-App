68d0d90e22021960fbd07384126fff7bdae1a52f
ram071985 <reidmuchow@gmail.com>
2020-04-11 14:09:45 -0500

    changed scripts.

---
+++ client/build/precache-manifest.1e45143eb9b5bc4162f18387af2995c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475f8cf778cc538d90ae67b41ff7b991",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79efd64de696b3aa5cb7",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c874ae24be91d2b3b27a",
+    "url": "/static/css/main.0dce260b.chunk.css"
+  },
+  {
+    "revision": "79efd64de696b3aa5cb7",
+    "url": "/static/js/2.33fc75b5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.33fc75b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c874ae24be91d2b3b27a",
+    "url": "/static/js/main.ed10523c.chunk.js"
+  },
+  {
+    "revision": "9dad8fe17a2b0719dc0a",
+    "url": "/static/js/runtime-main.96f2b29e.js"
+  }
+]);
