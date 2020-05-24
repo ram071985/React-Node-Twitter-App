78e7ac09e47d5f69ae764aa8c23cd7f96a1b0c9e
ram071985 <reidmuchow@gmail.com>
2020-05-24 18:09:43 -0500

    changed port to 3000 and README to have 1 random instead of 10 tweets.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = 3000; //process.env.PORT;
 const path = require("path");
 const axios = require("axios");
 
