4a26e4d6c83bc879da3eed45a2124e8d7f4a286e
ram071985 <reidmuchow@gmail.com>
2020-05-26 12:40:22 -0500

    readded donenv module in server component.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require('dotenv').config()
 const port = process.env.PORT;
 const path = require("path");
 const axios = require("axios");
